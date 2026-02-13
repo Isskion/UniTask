68cf7478fdf87437bf738a1e4a47e2dc4152b4e4
Daniel Del Amo <daniel.delamo@unigis.com>
2026-02-13 13:49:21 +0100

    chore: update 2026 Madrid holiday dates (translations for Sunday holidays)

--- lib/holidays.ts
+++ lib/holidays.ts
@@ -28,9 +28,9 @@ export const MADRID_HOLIDAYS = [
     "2026-05-15", // San Isidro
     "2026-08-15", // Asunción de la Virgen
     "2026-10-12", // Fiesta Nacional de España
-    "2026-11-01", // Todos los Santos
+    "2026-11-02", // Traslado de Todos los Santos
     "2026-11-09", // La Almudena
-    "2026-12-06", // Día de la Constitución Española
+    "2026-12-07", // Traslado del Día de la Constitución
     "2026-12-08", // Inmaculada Concepción
     "2026-12-25", // Natividad del Señor
 ];
