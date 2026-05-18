a879fb718613d687340da9829b04ff2c2e6a4aec
Jesus Marquez <jesus.marquez@unigis.com>
2026-05-18 17:16:30 +0200

    fix: update validateOrderRow type signature

--- app/uniclientcreator/_src/components/DataPanel/MasterTable.tsx
+++ app/uniclientcreator/_src/components/DataPanel/MasterTable.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+﻿/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useMemo, useCallback, useState, useRef } from 'react';
 import { useAppStore } from '../../store/appStore';
 
